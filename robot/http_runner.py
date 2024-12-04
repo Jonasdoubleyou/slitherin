5f5f51e6354e287b51bb93b7dd7d180bf45ac398
Jonas Wilms <jonas@wilms.ninja>
2024-12-04 14:49:20 +0100

    fix: Set CORS headers properly

--- robot/http_runner.py
+++ robot/http_runner.py
@@ -3,6 +3,12 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 PATH = "/home/robot/robot/"
 
 class WebServer(BaseHTTPRequestHandler):
+    def set_cors(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+
     def do_GET(self):
         if self.path == "/status":
             self.get_status()
@@ -14,8 +20,15 @@ class WebServer(BaseHTTPRequestHandler):
         if self.path == "/command":
             self.post_command()
 
+    def do_OPTIONS(self):
+        """ Answer CORS request """
+        self.send_response(200, "ok")
+        self.set_cors()
+        self.end_headers()
+        
     def get_index(self):
         self.send_response(200)
+        self.set_cors()
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
 
@@ -24,6 +37,7 @@ class WebServer(BaseHTTPRequestHandler):
 
     def get_status(self):
         self.send_response(200)
+        self.set_cors()
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
 
@@ -36,6 +50,7 @@ class WebServer(BaseHTTPRequestHandler):
         with open(PATH + "command.json", "w") as f:
             f.write(command)
         self.send_response(200)
+        self.set_cors()
         self.end_headers()
         self.wfile.write("OK".encode(encoding='utf_8'))
 
